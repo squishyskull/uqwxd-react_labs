1dd23b92ea8f529d29e081c18d39d45b3bbd22c7
squishyskull <tamara.e.brooks1@gmail.com>
2023-02-20 01:09:49 -0500

    ok

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
+
+import {legacy_createStore as createStore} from 'redux';;
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
 
@@ -17,8 +17,3 @@ myStore.subscribe(()=>console.log(myStore.getState()));
 //Enveloping the App inside the Provider, ensures that the states in the store are available
 //throughout the application
 ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
